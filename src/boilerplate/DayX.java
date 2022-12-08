995125703569a0e7c0279cdbe284a3d9d82b0d05
krishanudb <krishanudb@gmail.com>
2022-12-08 09:45:41 +0530

    Clean up boilerplate

--- src/boilerplate/DayX.java
+++ src/boilerplate/DayX.java
@@ -4,13 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DayX {
+    private static final List<String> input = new ArrayList<>();
+
     public static void main(String[] args) {
+        consumeInput();
+        partOne();
+        partTwo();
+        // TODO: Print output
+    }
+
+    private static void partOne() {
+        // TODO: Process input
+    }
+
+    private static void partTwo() {
+        // TODO: Process input
+    }
+
+    private static void consumeInput() {
         try (BufferedReader reader = new BufferedReader(new FileReader("src/dayx/input.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                // TODO: Process line
+                input.add(line);
             }
         } catch (FileNotFoundException e) {
             System.err.println("Failed to open file: " + e);
